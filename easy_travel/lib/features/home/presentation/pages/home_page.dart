999f220e232aaea11048a1d4a07e96927a7a1315
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-04 21:14:12 -0500

    refactor(home): update Status enum import

--- easy_travel/lib/features/home/presentation/pages/home_page.dart
+++ easy_travel/lib/features/home/presentation/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:easy_travel/core/enums/status.dart';
 import 'package:easy_travel/features/home/domain/category.dart';
 import 'package:easy_travel/features/home/domain/destination.dart';
 import 'package:easy_travel/features/home/presentation/blocs/home_bloc.dart';
