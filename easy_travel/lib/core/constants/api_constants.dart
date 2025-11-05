8040fad6b0b32d2e79e60e38c1a71b6bf24bab1b
jmayta1984 <jmayta@pucp.edu.pe>
2025-11-04 21:07:50 -0500

    feat(core): add ApiConstants for centralized API endpoints

---
+++ easy_travel/lib/core/constants/api_constants.dart
@@ -0,0 +1,6 @@
+class ApiConstants {
+  static final String baseUrl =
+      'https://destinationapp-h4e8dvace3fqffbb.eastus-01.azurewebsites.net';
+  static final String destinationsEndpoint = '/api/destinations';
+  static final String loginEndpoint = '/api/destinations';
+}
